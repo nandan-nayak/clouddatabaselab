c243d196e79d08a6ace905d6f16d3413dfb74edf
nandan-nayak <44666958+nandan-nayak@users.noreply.github.com>
2020-01-08 13:45:15 +0530

    Update mysqldbconnection.py

--- complete/mysqldbconnection.py
+++ complete/mysqldbconnection.py
@@ -1,6 +1,6 @@
-﻿import sqlite3
+import sqlite3
 from sqlite3 import Error
-
+import customexception as excep
 DBNAME="sharemarket.db"
 createtablesql="create table if not exists companyinfo(id integer primary key autoincrement,companyname text,sharevalue real)"   
 
@@ -35,60 +35,73 @@ def createtable():
 def insertvalues(companyname,sharevalue):
     sql="insert into companyinfo(id,companyname,sharevalue) values(null,'"+companyname+"',"+str(sharevalue)+")";
     con,cursor=connect()
-    print(sql)
+    #print(sql)
+    message=""
     try:
         #print("inserted")
         cursor.execute(sql)
         con.commit()
+        message="values  inserted successfully"
     except Error as e:
         print(e)
         con.rollback()
     finally:
         con.close();
-    #print(sql)
+    return message
 
 def deleteinfo(id):
     sql="delete from companyinfo where id="+str(id)
     con,cursor=connect()
+    message=""
     try:
         #print("deleted")
         cursor.execute(sql)
         con.commit();
+        message="deleted successfully"
     except Error as e:
         print(e)
         conn.rollback();
+        message="failed to delete ... "
     finally:
         con.close();
+    return message
 
 
 def updateinfo(id,sharevalue):
     sql="update companyinfo set sharevalue="+str(sharevalue)+" where id="+str(id);
     con,cursor=connect();
+    message=""
     try:
         #print("updated")
         cursor.execute(sql)
         con.commit();
+        message="updated successfully"
     except Error as e:
         print(e)
         conn.rollback();
+        message="updation Failed"
     finally:
         con.close();
+    return message
 
-def display(id):
-    sql="select * from companyinfo where id="+str(id)
+def display(name):
+    sql="select * from companyinfo where companyname="+str(name)
     con,cursor=connect();
     result=None
+    message=""
     try:
         #print("displayed")
         cursor.execute(sql)
         result=cursor.fetchall()
         #con.commit()
+        message="company found"
     except Error as e:
         print(e);con.rollback();
+        message="company not found"
     finally:
         con.close();
     
-    return result
+    return result,message
     
         
 
@@ -113,8 +126,3 @@ for row in result:
 
 deleteinfo(3)
 '''
-
-
-
-
-
