2ee1f40419c11fe47b0cbdeaba689a8480f5e1cf
nandan-nayak <44666958+nandan-nayak@users.noreply.github.com>
2020-01-08 13:43:58 +0530

    Update main.py

--- complete/main.py
+++ complete/main.py
@@ -1,5 +1,5 @@
 import eel
-import mysqldbconnection
+import mysqldbconnection as db
 eel.init('web')
 options = {
     'mode': "chrome", #or "chrome-app",
@@ -8,21 +8,34 @@ options = {
     'chromeFlags': ["--start-fullscreen", "--browser-startup-dialog"]
 }
 
+db.createtable();
 @eel.expose
 def add(companyname,value):
+    
+    
+    message=db.insertvalues(companyname,value)
+    eel.added(message)
     print(companyname+value)
+    
 
 
 @eel.expose
 def modify(id, value):
+    message=db.updateinfo(id,value)
+    eel.modified(message)
     print(id + value)
 
 @eel.expose
-def display(id):
-    print(id)
+def display(name):
+    result,message=db.display(name)
+    eel.display(message)
+    #print(id)
 
 @eel.expose
 def deletedetails(id):
+    message=db.delete(id)
+    print(message)
+    eel.delete(message)
     print(id)
 
 
@@ -32,7 +45,12 @@ def deletedetails(id):
 
 eel.my_javascript_function('Hello ', 'world!') 
 
+eel.start('main.html',options=options,suppress_error=True)
+
+'''
 try:
     eel.start('main.html',options=options)
 except:
+    print("er")
     pass
+'''
