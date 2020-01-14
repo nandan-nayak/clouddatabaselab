9bbca5616de1e6e673de740c8586a0845850ea3c
nandan-nayak <44666958+nandan-nayak@users.noreply.github.com>
2020-01-14 12:32:41 +0530

    Update install.py

--- complete/install.py
+++ complete/install.py
@@ -1,3 +1,10 @@
+"""
+This class installs the required package to run the application which is specified in "requirement.txt" file
+You  need to have python3.5+ installed in the system
+
+"""
+
+
 import os
 
 os.system("pip install -r requirements.txt")
