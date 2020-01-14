ca5e665623ad1a5e0a7e946908368c59abf9e66f
nandan-nayak <44666958+nandan-nayak@users.noreply.github.com>
2020-01-14 12:29:34 +0530

    Update customexception.py

--- complete/customexception.py
+++ complete/customexception.py
@@ -1,3 +1,12 @@
+"""
+This class Provides Custom Exception for the Database
+
+import this class and "raise the Exception" to throw the Exception 
+while catching the Exception call the particular function 
+
+"""
+
+
 class IDNotFoundException(Exception):
     def errormessage():
         return "Company ID not found"
